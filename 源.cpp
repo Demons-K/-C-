0f47a6f2ee2a85ea9b6b365462761be9d7929df8
wsmbsbbz <1037214973@qq.com>
2021-04-15 12:16:48 +0800

    格式化

--- 源.cpp
+++ 源.cpp
@@ -1,31 +1,28 @@
-#include<graphics.h>//图形库头文件
-#include<mmsystem.h>//多媒体设备头文件
-#include<math.h>//判断落点要用到
-#pragma comment(lib,"winmm.lib")//播放音乐需要使用的库文件
+#include <graphics.h>			  //图形库头文件
+#include <mmsystem.h>			  //多媒体设备头文件
+#include <math.h>				  //判断落点要用到
+#pragma comment(lib, "winmm.lib") //播放音乐需要使用的库文件
 
-int flag = 0;//表示下棋次数
-int board[20][20] = { 0 };//防止重叠下棋
+int flag = 0;			 //表示下棋次数
+int board[20][20] = {0}; //防止重叠下棋
 
-
-
-
-void initGame()//初始化游戏
+void initGame() //初始化游戏
 {
 	initgraph(600, 500); //绘制窗口
 
-	loadimage(NULL, "背景2.jpg");  //加载背景图片
+	loadimage(NULL, "背景2.jpg"); //加载背景图片
 
-	mciSendString("open Mysoul.mp3", 0, 0, 0);//多媒体控制接口
+	mciSendString("open Mysoul.mp3", 0, 0, 0); //多媒体控制接口
 	//暂时关掉音乐mciSendString("play Mysoul.mp3", 0, 0, 0);
 
 	//设置线条颜色
 	setlinecolor(BLACK);
 
 	//绘制棋盘
-	for (int i = 25; i<=500;i+=25)
-	{ 
+	for (int i = 25; i <= 500; i += 25)
+	{
 		line(i, 0, i, 500);
-		line(0, i,500,i);
+		line(0, i, 500, i);
 	}
 	line(501, 0, 501, 500);
 
@@ -41,23 +38,21 @@ int Judge(int, int);
 void Playchess()
 {
 	//鼠标
-	MOUSEMSG mouse;//保存鼠标消息的结构体
+	MOUSEMSG mouse; //保存鼠标消息的结构体
 
 	int x, y;
 	int a, b;
 
 	while (1)
 	{
-		
 
-		mouse = GetMouseMsg();//获取鼠标消息
-
+		mouse = GetMouseMsg(); //获取鼠标消息
 
 		for (int i = 1; i < 20; i++)
 		{
 			for (int j = 1; j < 20; j++)
 			{
-				if (abs(mouse.x-i*25)<12 && abs(mouse.y-j*25)<12)
+				if (abs(mouse.x - i * 25) < 12 && abs(mouse.y - j * 25) < 12)
 				{
 					x = i * 25;
 					y = j * 25;
@@ -65,35 +60,30 @@ void Playchess()
 					b = j;
 				}
 			}
-
-
 		}
 		if (mouse.uMsg == WM_LBUTTONDOWN)
 		{
 
-			if (board[a][b] != 0)//检查防止重叠下棋
+			if (board[a][b] != 0) //检查防止重叠下棋
 			{
-				MessageBox(NULL, "这里已经有棋子了，请重新选择", "提示", MB_OK);//给出提示
+				MessageBox(NULL, "这里已经有棋子了，请重新选择", "提示", MB_OK); //给出提示
 
-				continue;//推出循环重新进入
+				continue; //推出循环重新进入
 			}
 
-
-			if (flag % 2 == 0)//偶数的情况
+			if (flag % 2 == 0) //偶数的情况
 			{
 				setfillcolor(BLACK);
 				solidcircle(x, y, 10);
 				board[a][b] = 1;
-
 			}
 			else
 			{
 				setfillcolor(WHITE);
 				solidcircle(x, y, 10);
 				board[a][b] = 2;
-
 			}
-			
+
 			flag++;
 		}
 
@@ -107,12 +97,9 @@ void Playchess()
 			else
 			{
 				MessageBox(NULL, "周庆钊胜利", "游戏结束", MB_OK);
-					exit(0);	
-			
+				exit(0);
 			}
-
 		}
-
 	}
 }
 
@@ -143,14 +130,12 @@ int Judge(int a, int b)
 	return 0;
 }
 
-
 int main()
 {
 	initGame();
 	Playchess();
 
-
-	while (1);
+	while (1)
+		;
 	return 0;
-
 }
