9f39fec9f72a8d6a794d6b284f2fd0750119f4cb
wsmbsbbz <1037214973@qq.com>
2021-05-01 22:39:52 +0800

    version 1.0

--- 源.cpp
+++ 源.cpp
@@ -4,7 +4,7 @@
 #pragma comment(lib,"winmm.lib")//播放音乐需要使用的库文件
 
 int flag = 0;//表示下棋次数
-int board[20][20] = { 0 };//防止重叠下棋
+int board[30][20] = { 0 };//防止重叠下棋
 int m = 0;
 int n = 0;
 
@@ -25,25 +25,69 @@ void initGame()//初始化游戏
 	setlinecolor(BLACK);
 
 	//绘制棋盘
-	for (int i = 25; i<=500;i+=25)
+	for (int i = 25; i<=475;i+=25)
 	{ 
-		line(i, 0, i, 500);
-		line(0, i,500,i);
+		line(i, 25, i, 475);
+		line(25, i,475,i);
 	}
 	line(501, 0, 501, 500);
 
 	//输出文字
-	outtextxy(505, 50, "玩家一：白棋");
-	outtextxy(505, 100, "玩家二：黑棋");
-	outtextxy(505, 200, "规则：");
-	outtextxy(505, 230, "1.黑棋先行");
-	outtextxy(505, 260, "2.右键悔棋");
+	setbkmode(TRANSPARENT);
+	setfillcolor(RGB(116, 138, 152));
+	solidcircle(550, 90, 25);
+	solidcircle(550, 190, 25);
+	solidcircle(550, 280, 25);
+	solidcircle(555, 370, 25);
+	solidcircle(555, 460, 25);
 
+	outtextxy(537, 80, "悔棋");
+	outtextxy(537, 180, "重开");
+	outtextxy(537, 270, "帮助");
+	outtextxy(540, 360, "静音");
+	outtextxy(540, 450, "播放");
 }
 
 //下棋
 //判断输赢的函数
 int Judge(int, int);
+
+void Remake(void)//重开一局
+{
+	cleardevice();
+	initGame();
+	for (int i = 0; i < 30; i++)
+		for (int j = 0; j < 20; j++)
+			board[i][j] = 0;
+}
+
+void Huiqi(void)//悔棋
+{
+	board[m][n] = 0;
+	cleardevice();
+	initGame();
+	for (int i = 1; i < 30; i++)
+	{
+		for (int j = 1; j < 20; j++)
+		{
+			if (board[i][j] == 1)
+			{
+				setfillcolor(BLACK);
+				solidcircle(i * 25, j * 25, 10);
+			}
+			else if (board[i][j] == 2)
+			{
+				setfillcolor(WHITE);
+				solidcircle(i * 25, j * 25, 10);
+			}
+
+		}
+	}
+}
+void Menu(void)
+{
+	MessageBox(NULL, "本游戏由曹瑞珂编写", "帮助", MB_OK);
+}
 
 void Playchess()
 {
@@ -62,34 +106,9 @@ void Playchess()
 
 		mouse = GetMouseMsg();//获取鼠标消息
 
-
-		if (mouse.uMsg == WM_RBUTTONDOWN)
+		for (int i = 1; i < 30; i++)
 		{
-			board[m][n] = 0;
-			cleardevice();
-			initGame();
-			for (int i = 0; i < 20; i++)
-			{
-				for (int j = 0; j < 20; j++)
-				{
-					if (board[i][j] == 1)
-					{
-						setfillcolor(BLACK);
-						solidcircle(i*25, j*25, 10);
-					}
-					else if(board[i][j] == 2)
-					{
-						setfillcolor(WHITE);
-						solidcircle(i*25, j*25, 10);
-					}
-
-				}
-			}
-		}
-
-		for (int i = 0; i < 20; i++)
-		{
-			for (int j = 0; j < 20; j++)
+			for (int j = 1; j < 20; j++)
 			{
 				if (abs(mouse.x-i*25)<12 && abs(mouse.y-j*25)<12)
 				{
@@ -105,28 +124,46 @@ void Playchess()
 		if (mouse.uMsg == WM_LBUTTONDOWN)
 		{
 
-			if (board[a][b] != 0)//检查防止重叠下棋
+			if (x > 500)
 			{
-				MessageBox(NULL, "这里已经有棋子了，请重新选择", "提示", MB_OK);//给出提示
-
-				continue;//推出循环重新进入
+				if (y > 55 && y < 110)
+					Huiqi();
+				if (y > 155 && y < 205)
+					Remake();
+				if (y > 245 && y < 300)
+					Menu();
+				if (y > 330 && y < 390)
+					mciSendString("pause Mysoul.mp3", 0, 0, 0);
+				if (y > 420 && y < 480)
+					mciSendString("play Mysoul.mp3", 0, 0, 0);
 			}
-
-
-			if (flag % 2 == 0)//偶数的情况
+			else if(x < 500)
 			{
-				setfillcolor(BLACK);
-				solidcircle(x, y, 10);
-				board[a][b] = 1;
+				if (board[a][b] != 0)//检查防止重叠下棋
+				{
+					MessageBox(NULL, "这里已经有棋子了，请重新选择", "提示", MB_OK);//给出提示
+
+					continue;//推出循环重新进入
+				}
+
+
+				if (flag % 2 == 0)//偶数的情况
+				{
+					setfillcolor(BLACK);
+					solidcircle(x, y, 10);
+					board[a][b] = 1;
+
+				}
+				else
+				{
+					setfillcolor(WHITE);
+					solidcircle(x, y, 10);
+					board[a][b] = 2;
+				}
 
 			}
-			else
-			{
-				setfillcolor(WHITE);
-				solidcircle(x, y, 10);
-				board[a][b] = 2;
-
-			}
+			
+			
 			m = a;
 			n = b;
 			
@@ -139,12 +176,12 @@ void Playchess()
 		{
 			if (flag % 2 == 0)
 			{
-				MessageBox(NULL, "玩家一胜利", "游戏结束", MB_OK);
+				MessageBox(NULL, "白棋胜利", "游戏结束", MB_OK);
 				exit(0);
 			}
 			else
 			{
-				MessageBox(NULL, "玩家二胜利", "游戏结束", MB_OK);
+				MessageBox(NULL, "黑棋胜利", "游戏结束", MB_OK);
 				exit(0);	
 			
 			}
@@ -154,7 +191,7 @@ void Playchess()
 	}
 }
 
-	int Judge(int a, int b)
+	int Judge(int a, int b)//判断游戏是否结束
 	{
 		int i, j;
 		int t = 2 - flag % 2;
